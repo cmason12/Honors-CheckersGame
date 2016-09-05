e85dfbe8b798fe1c11f4c34a3705056e53897c12
Chase Mason <siegthefool@gmail.com>
2016-09-05 13:43:12 -0400

    Create CheckerPiece.java

---
+++ CheckerPiece.java
@@ -0,0 +1,11 @@
+package checkersgame;
+
+import javafx.scene.image.Image;
+import javafx.scene.shape.Circle;
+
+public class CheckerPiece {
+
+    String name;
+    Circle piece;
+    Image pattern;
+}
